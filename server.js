66bab9dd7d1682ba2ccda54bc8314596b5c725c2
Jari Saukkonen <jari.saukkonen@ambientia.fi>
2020-03-29 00:14:20 +0200

    Code for a working relay

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 'use strict'
 
 const ruuvi = require('node-ruuvitag');
+const config = require('./config');
+const request = require('request');
 
 ruuvi.on('found', tag => {
     console.log('Found RuuviTag ' + JSON.stringify(tag));
     tag.on('updated', tagdata => {
 	console.log(tag.id + ' ' + JSON.stringify(tagdata));
+	let data = `Ruuvi ${tag.id}.temperature=${tagdata.temperature},${tag.id}.pressure=${tagdata.pressure/100.0},${tag.id}.humidity=${tagdata.humidity}`;
+
+	request.post({
+       	    url: config.endpoint,
+            qs: {
+            	db: config.db,
+            	precision: 'ms'
+            },
+            auth: { 'username': config.username, 'password': config.password },
+            body: data
+        }, (error, response, body) => {
+            if (!error && response.statusCode == 204) {
+                console.log('Sent ' + data);
+            } else {
+                console.log('Error: ' + JSON.stringify(body) + ' data was ' + data);
+            }
+        });
     });
 });
